375362a51c181e4669b7ae0ab0dd011a197f2291
*WkMWuglGm9 <k-yamamoto@pscsrv.co.jp>
2022-02-22 19:55:06 +0900

    ハンズオン完了

--- pages/index.vue
+++ pages/index.vue
@@ -37,17 +37,50 @@
         No other internal dependency
       </card>
     </div>
+    <div class="component-space">
+      <div class="gazo-box">
+        <square-com first-color="red" second-color="green"/><br>
+        <square-com first-color="blue" second-color="yellow"/><br>
+        <square-com first-color="black" second-color="purple"/><br>
+        <square-com first-color="gray" second-color="white"/><br>
+        <square-com first-color="lightgreen" second-color="cyan"/><br>
+      </div>
+
+      <div class="gazo-box">
+        <square-com first-color="orange" second-color="purple"/><br>
+        <square-com first-color="lightblue" second-color="#CEF9DC"/><br>
+        <square-com first-color="cyan" second-color="purple"/><br>
+        <square-com first-color="fuchsia" second-color="red"/><br>
+        <square-com first-color="purple" second-color="olive"/><br>
+      </div>
+    </div>
+
   </section>
 </template>
 
 <script>
 import Card from '~/components/Card'
+import SquareCom from '~/components/SquareCom'
 
 export default {
   name: 'HomePage',
 
   components: {
-    Card
-  }
-}
+    Card,
+    SquareCom,
+  },
+
+}  
 </script>
+
+<style type="text/css">
+  div.gazo-box {
+    float: left;
+    padding-right: 200px;
+    justify-content: center;
+  }
+
+  div.component-space{
+    padding: 80px 150px;
+  }
+</style>
